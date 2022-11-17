52d99b3b02b27d2362238709a42e2a1750457e33
Maxime-Missichini <themynder@gmail.com>
2022-11-16 19:54:27 -0500

    Client page rework

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
  <div class="col-md-12">
   <br />
-  <h3 align="center">Liste des clients</h3>
+  <h3 align="center">Liste des clients résidentiels</h3>
   <br />
   @if($message = Session::get('success'))
   <div class="alert alert-success">
@@ -13,7 +13,6 @@
   </div>
   @endif
   <div align="right">
-   <a href="{{route('client.create')}}" class="btn btn-primary">Add</a>
    <br />
    <br />
   </div>
@@ -21,8 +20,6 @@
    <tr>
     <th>First Name</th>
     <th>Last Name</th>
-    <th>Edit</th>
-    <th>Delete</th>
    </tr>
    @foreach($clients as $row)
    <tr>
@@ -30,29 +27,10 @@
     <td>{{$row['last_name']}}</td>
     <td><a href="{{route('client.edit',['id'=> $row['id']])}}" class="btn btn-warning">Edit</a></td>
     <td>
-     <form method="post" class="delete_form" action="{{route('client.destroy',['id'=> $row['id']])}}">
-      {{csrf_field()}}
-      <input type="hidden" name="_method" value="DELETE" />
-      <button type="submit" class="btn btn-danger">Delete</button>
-     </form>
     </td>
    </tr>
    @endforeach
   </table>
  </div>
 </div>
-<script>
-$(document).ready(function(){
- $('.delete_form').on('submit', function(){
-  if(confirm("Voulez-vous vraiment supprimer ce client?"))
-  {
-   return true;
-  }
-  else
-  {
-   return false;
-  }
- });
-});
-</script>
 @endsection
