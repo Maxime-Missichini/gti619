a51c8c984d0e9c48fdde631908bedbb4afedf6c2
Maxime-Missichini <themynder@gmail.com>
2022-12-04 16:12:46 -0500

    Added an icon for the tab :)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>GTI619 LAB5</title>
+    <title>GTI619 - LAB 5</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://e7.pngegg.com/pngimages/786/101/png-clipart-password-computer-icons-security-safety-icon-safety-icon-child-safety-lock-thumbnail.png" rel="icon">
 </head>
 <body>
 
